5df515c614128f271db703fff8ce5194519228f0
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-12 23:06:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,20 +2,11 @@
 #include "lib.h"
 
 using namespace std;
-float massimo ( float a [], int d) {
-    cin >> d;
-    if (d>0){
-        for (int i=0; i<d; i++) {
-            cin >> a[i];
+float massimo (float b [], int d) {
+    float max=b[0];
+    for (int i=0; i<d; i++){
+        if (b[i]>max){
+            max=b[i];
         }
-    }
-float max=0; int p;
-a[0]=max;
-p=0;
-for (int i=0; i<d; i++){
-    if (a[i]>max) {
-    max=a[i];
-    p=i;
-    }
-} return max;
+    } return max;
 }
